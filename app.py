442657f898604bfa00aeda9a366999b2c6e50131
usapecrise <usapecrise@gmail.com>
2026-02-10 11:42:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 from datetime import date
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
+AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
+AIRTABLE_TABLE_NAME = os.getenv("AIRTABLE_TABLE_NAME")
 
 # =========================================================
 # APPLICATION METADATA
